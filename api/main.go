a379d42c66ec368ea519f6fc238e7efd3ef6a6d2
olssonik <borozytoja@gmail.com>
2024-08-21 23:03:52 +0100

    play a random video every time

--- api/main.go
+++ api/main.go
@@ -1,39 +1,45 @@
 package main
 
 import (
+    "fmt"
     "io"
     "net/http"
     "os"
+    "math/rand"
+    "time"
 )
 
 func videoHandler(w http.ResponseWriter, r *http.Request) {
-    videoFile, err := os.Open("videos/test3.mp4")
+    videoFiles, err := os.ReadDir("videos/")
+    if err != nil {
+        http.Error(w, "Error reading video directory.", http.StatusInternalServerError)
+        return
+    }
+
+    numFiles := len(videoFiles)
+
+    randomInt := rand.Intn(numFiles)
+
+    selectedFile := videoFiles[randomInt].Name()
+    videoFilePath := fmt.Sprintf("videos/%s", selectedFile)
+
+    videoFile, err := os.Open(videoFilePath)
     if err != nil {
         http.Error(w, "Video not found.", http.StatusNotFound)
         return
     }
     defer videoFile.Close()
-    // Set appropriate cache-control headers to prevent browser caching
+
     w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
     w.Header().Set("Pragma", "no-cache")
     w.Header().Set("Expires", "0")
 
-    // Set CORS headers
     w.Header().Set("Access-Control-Allow-Origin", "*")
     w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
     w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
     w.Header().Set("Content-Type", "video/mp4")
 
-    // Read the entire video file into memory
-    videoData, err := io.ReadAll(videoFile)
-    if err != nil {
-        http.Error(w, "Error reading video file.", http.StatusInternalServerError)
-        return
-    }
-
-    // Write the video data directly to the response writer
-    _, err = w.Write(videoData)
+    _, err = io.Copy(w, videoFile)
     if err != nil {
         http.Error(w, "Error writing video data.", http.StatusInternalServerError)
         return
@@ -41,6 +47,8 @@ func videoHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    rand.Seed(time.Now().UnixNano())
+
     http.HandleFunc("/video", videoHandler)
     http.ListenAndServe("0.0.0.0:8080", nil)
 }
